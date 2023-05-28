6adfa1c45bc1d68cacbef4f37b1f040bd716cfe6
ormima <92382637+ormima@users.noreply.github.com>
2023-05-28 22:06:44 +0200

    Add files via upload

---
+++ RybaProjekt-MateuszPilszczek/wedkuj.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl_1.css">
+    <title>Wędkowanie</title>
+</head>
+<body>
+<header id="baner">
+    <h1>Portal dla wędkarzy</h1>
+</header>
+
+<div id="lewe">
+<section class="lewy lewy1">
+    <h3>Ryby zamieszkujące rzeki</h3>
+    <ol id="skrypt1">
+        <li>ABC</li>
+        <li>ABC</li>
+        <li>ABC</li>
+    </ol>   
+</section>
+
+
+<section class="lewy lewy2">
+    <h3>Ryby drapieżne naszych wód</h3>
+    <table>
+        <th>
+            L.p.
+        </th>
+        <th>
+            Gatunek
+        </th>
+        <th>
+            Występowanie
+        </th>
+
+        <tr>
+            <td>Liczba </td>
+            <td> .$Gatunek </td>
+            <td> .$wystepowanie </td>
+        </tr>
+
+    </table>
+</section>
+</div>
+
+<div id="prawe">
+<section class="prawy">
+    <img src="./ryba1.jpg" alt="Sum">
+    <br>
+    <a href="./kwerendy.txt" download>Pobierz kwerendy </a>
+</section>
+</div>
+
+
+<footer>
+    <p>Stronę wykonał: XYZ123</p>
+</footer>
+</body>
+</html>
